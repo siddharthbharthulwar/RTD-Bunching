3309eb3fcb1a1c87e318028442e6d62a822d1ecc
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-06-20 21:41:51 -0600

    add stop points (useless for localization)

--- rtd-data/data-processing/shapes.py
+++ rtd-data/data-processing/shapes.py
@@ -1,12 +1,29 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
 import cv2 as cv
+import numpy as np
 
 df = pd.read_csv(r"rtd-data\res\shapes.txt")
 
 a = df.groupby('shape_id')[['shape_pt_lat', 'shape_pt_lon', 'shape_pt_sequence', 'shape_dist_traveled']].apply(lambda g: g.values.tolist()).to_dict()
 img = cv.imread('rtd-data\data-processing\map.png', cv.IMREAD_COLOR)
 
+de = pd.read_csv(r"rtd-data\res\stops.txt")
+
+stoplats = []
+stoplons = []
+colors = []
+
+for index, row in de.iterrows():
+
+    stoplats.append(row['stop_lat'])
+    stoplons.append(row['stop_lon'])
+
+for i in range(0, len(stoplats)):
+
+    colors.append(255)
+
+colors = np.array(colors)
 
 for key in a:
 
@@ -22,4 +39,5 @@ for key in a:
 
     plt.imshow(img[:,:,::-1], extent = [-105.2888, -104.6613, 39.5401, 40.0476])
     plt.plot(lons, lats)
+    plt.scatter(stoplons, stoplats, c = colors , alpha = 0.5)
     plt.show()
