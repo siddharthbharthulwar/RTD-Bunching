741bb8625f054bbfae608949d6307d37d589aa34
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-07-02 12:16:30 -0600

    add 1d heatmap

--- rtd-data/data-processing/secondary.py
+++ rtd-data/data-processing/secondary.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from shapes import Shape, ShapeReader
 from formulas import haversine
 import cv2 as cv
+import numpy as np
+import seaborn as sns
 
 class BunchingInstance():
 
@@ -25,6 +27,10 @@ class Route:
         self.shape = shape
         self.bunching_instances = []
 
+    def calculate_percentage_down_route(self, bunching_instance):
+
+        index_one, index_two = self.locate_two_closest_points(bunching_instance)  
+
     def locate_two_closest_points(self, bunchinginstance):
 
         main_lat = bunchinginstance.latitude
@@ -45,7 +51,7 @@ class Route:
         
         pt_one = shapes_duplicated[mindex]
         shapes_duplicated.pop(mindex)
-
+        index_one = mindex
         mindex = 0
         mindist = haversine(main_lat, main_lon, shapes_duplicated[0][0], shapes_duplicated[0][1])
 
@@ -60,11 +66,42 @@ class Route:
         
         pt_two = shapes_duplicated[mindex]
 
-        return pt_one, pt_two
+        return index_one, mindex
+
+    def initialize_table(self):
+
+        self.table = pd.DataFrame(columns = ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'])
+            
+        ls = np.zeros(24).tolist()
+        a_series = pd.Series(ls, index = self.table.columns)
+        self.table = self.table.append(a_series, ignore_index=True)
+
+    def add_to_table(self, bunchinginstance):
+
+        columnvals = ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM']
+        timestampvals = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12' '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'] 
+
+        if (len(str(bunchinginstance.timestamp)) == 2):
+
+            column_index = '12 AM' 
+        else:
+
+            column_index = columnvals[timestampvals.index(str(bunchinginstance.timestamp)[:2])]
+        self.table.iloc[0, self.table.columns.get_loc(column_index)] +=1
+
+    def show_table(self):
+
+        plt.pcolor(self.table, cmap = 'Blues')
+        plt.xlim((0, 24))
+        plt.ylim((0, 1))
+        plt.title(self.routeid)
+        plt.show()
 
 
 class BunchingReader:
 
+
+    
     def __init__(self, datestr):
 
         dd = pd.read_csv("rtd-data/data-processing/data/realShapes.csv")
@@ -98,7 +135,8 @@ class BunchingReader:
         for item in self.routes:
 
             rt = self.routes[item]
-
+            rt.initialize_table()
+            
             routeX = []
             routeY = []
 
@@ -107,41 +145,8 @@ class BunchingReader:
 
             for bi in rt.bunching_instances:
 
-
-
-                routeX.append(bi.longitude)
-                routeY.append(bi.latitude)
-
-            plt.scatter(routeX, routeY)
-            plt.title(str(item))
-            plt.show()
-                
-            '''
-            img = cv.imread('rtd-data\data-processing\map.png', cv.IMREAD_COLOR)
-            plt.imshow(img[:,:,::-1], extent = [-105.2888, -104.6613, 39.5401, 40.0476])
-
-            la, lb = rt.locate_two_closest_points(bi)
-
-            totalX = []
-            totalY = []
-
-            for ab in rt.shape:
-
-                totalX.append(ab[1])
-                totalY.append(ab[0])
-
-            plt.scatter(totalX, totalY)
-
-            plt.scatter(bi.longitude, bi.latitude)
-            plt.scatter(la[1], la[0])
-            plt.scatter(lb[1], lb[0])
-            titleStr = bi.asString() + " route " + item
-            plt.title(titleStr)
-            plt.show()
-
-            '''
-
+                rt.add_to_table(bi)
+            
+            rt.show_table()
+        
 b = BunchingReader("06-19-20")
-rt = b.routes['FF1']
-
-la, lb = rt.locate_two_closest_points(rt.bunching_instances[1])
